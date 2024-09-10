cd67ac82014715362bf53adb0e1d9ab5d44ff740
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-10 12:26:52 +0200

    add layout menu bar

--- src/app/core/base-layout/base-layout.component.ts
+++ src/app/core/base-layout/base-layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MENU_ITEMS } from '../menu.config';
 
 @Component({
   selector: 'app-base-layout',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './base-layout.component.html',
-  styleUrl: './base-layout.component.scss'
+  styleUrl: './base-layout.component.scss',
 })
 export class BaseLayoutComponent {
-
+  protected readonly menuItems = MENU_ITEMS;
 }
