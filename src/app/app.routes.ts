07c2fbadf43798fbe5059e98eaec60b87a95b321
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-10 11:26:04 +0200

    create base layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BaseLayoutComponent } from './core/base-layout/base-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [],
+  },
+];
