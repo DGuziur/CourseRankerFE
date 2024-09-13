5e4ad60dd0d50fca389f2fb319d9d5df37be6434
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 14:30:31 +0200

    create overview tab in details

--- src/app/features/courses-list/pages/course-details/course-details.component.ts
+++ src/app/features/courses-list/pages/course-details/course-details.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-course-details',
   standalone: true,
-  imports: [ButtonModule, TabViewModule],
+  imports: [ButtonModule, DividerModule, TabViewModule],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.scss',
 })
 export class CourseDetailsComponent {
-  activeIndex: number = 0;
-
   score: number = 8.65;
+  creators: string[] = ['Creator 1', 'Creator 2', 'Creator 3'];
+  tags: string[] = ['Tag 1', 'Tag 2', 'Tag 3'];
 }
